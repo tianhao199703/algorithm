05ccbe4c833617aed2fe1db29f9f7bafa92420ee
do-not-ask <aatthh1971@gmail.com>
2020-04-21 23:27:33 +0800

    Update quick_sort.cc

--- sort/quick_sort.cc
+++ sort/quick_sort.cc
@@ -25,3 +25,18 @@ void quick_sort(vector<int> a){
 	int low = 0, high = a.size() - 1;
 	quicksort(a, low, high);
 }
+//iterate version
+void quick_sort_iterate(vector<int> &a){
+	int low = 0, high = a.size() - 1;
+	queue<pair<int, int>> tmp;
+	tmp.push({ low, high });
+	while (!tmp.empty()){
+		auto cur = tmp.front();
+		tmp.pop();
+		if (cur.first < cur.second){
+			int mid = split(a, cur.first, cur.second);
+			tmp.push({ cur.first, mid - 1 });
+			tmp.push({ mid + 1, cur.second });
+		}
+	}
+}
