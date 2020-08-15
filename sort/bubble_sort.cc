499d270c0b658dfc16dc4e2b67736ca736b83129
do-not-ask <aatthh1971@gmail.com>
2020-08-15 14:17:39 +0800

    Update bubble_sort.cc
    优化了一下

--- sort/bubble_sort.cc
+++ sort/bubble_sort.cc
@@ -1,10 +1,14 @@
-void bubble_sort(vector<int> a){
+void bubble_sort(vector<int> &a){
 	int n = a.size();
+	bool neverSwap = false;
 	for (int i = 0; i < n; i++){
 		for (int j = n-1; j >= i+1;j--){
 			if (a[j] < a[j - 1]){
+				neverSwap = true;
 				swap(a[j], a[j - 1]);
 			}
+			if(neverSwap) return;
+			neverSwap = false;
 		}
 	}
 }
